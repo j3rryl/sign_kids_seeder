d26d85704ce4c4b1da475d96193d1cd72a23ae11
j3rryl <jeremymunroe0@gmail.com>
2025-03-28 09:00:25 +0300

    push

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,18 @@
+/* eslint-disable comma-dangle */
 /* eslint-disable no-unused-vars */
 /* eslint-disable object-curly-spacing */
 /* eslint-disable indent */
+
 const functions = require("firebase-functions");
 const nodemailer = require("nodemailer");
+require("dotenv").config();
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
 
 // Load email credentials from Firebase Config
-const user = functions.config().email.user;
-const pass = functions.config().email.pass;
+const user = process.env.USER_EMAIL;
+const pass = process.env.USER_PASS;
 
 // Gmail SMTP Setup
 const transporter = nodemailer.createTransport({
@@ -14,17 +20,27 @@ const transporter = nodemailer.createTransport({
   auth: { user, pass },
 });
 
-// Cloud Function to Send Email
-exports.sendEmail = functions.https.onCall((data, context) => {
+exports.sendEmail = functions.https.onCall(async (data, context) => {
+  const { email, subject, message } = data;
+
+  if (!email || !subject || !message) {
+    throw new functions.https.HttpsError(
+      "invalid-argument",
+      "Missing parameters"
+    );
+  }
+
   const mailOptions = {
-    from: user,
-    to: data.email,
-    subject: data.subject,
-    text: data.message,
+    from: "your-email@gmail.com",
+    to: email,
+    subject: subject,
+    text: message,
   };
 
-  return transporter
-    .sendMail(mailOptions)
-    .then(() => ({ success: true, message: "Email sent!" }))
-    .catch((error) => ({ success: false, message: error.toString() }));
+  try {
+    await transporter.sendMail(mailOptions);
+    return { success: true, message: "Email sent!" };
+  } catch (error) {
+    throw new functions.https.HttpsError("internal", error.toString());
+  }
 });
